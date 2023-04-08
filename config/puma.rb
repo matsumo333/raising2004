8c1c7639f6ce16e570c189387f915dc3bbf71eae
matsumo333 <matsumo333@icloud.com>
2023-04-08 21:06:58 +0900

    うごく状況に戻しました。

--- config/puma.rb
+++ config/puma.rb
@@ -17,7 +17,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 #
 port ENV.fetch("PORT") { 3000 }
 
-bind "unix:///var/www/raising2004/tmp/sockets/puma.sock"
+
 
 # Specifies the `environment` that Puma will run in.
 #
